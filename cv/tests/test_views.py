0e1056aa05a8c321a51c85448d5d4799f30d250b
Txy822 <tyohanis2018@gmail.com>
2020-07-15 18:34:33 +0100

    failing test_view method  for cv added

--- cv/tests/test_views.py
+++ cv/tests/test_views.py
@@ -1,1 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.urls import reverse
+from cv.views import cv_section_list
+
+class TestViews(TestCase):
+
+    def test_root_url_resolves_to_cv_page_view(self):
+        found = resolve('/cv')
+        self.assertEqual(found.func, cv_section_list)
