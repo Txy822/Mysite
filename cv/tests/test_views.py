7a7c4408fa62f28e6bc3bbcc0917f94a439a15fa
Txy822 <tyohanis2018@gmail.com>
2020-07-15 19:15:06 +0100

    failed unit l tests added

--- cv/tests/test_views.py
+++ cv/tests/test_views.py
@@ -1,9 +1,18 @@
 from django.test import TestCase,Client
 from django.urls import reverse,resolve
+from django.http import HttpResponse
 from cv.views import cv_section_list
 
 class TestViews(TestCase):
 
     def test_url_resolves_to_cv_page_view(self):
-        found = resolve('/cv')
+        found = resolve('/')
         self.assertEqual(found.func, cv_section_list)
+
+    def test_cv_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = cv_section_list(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
-def cv_section_list():
-    pass
+def cv_section_list(request):
+    return HttpResponse('<html><title>Curriculun vitae(CV)</title></html>')
