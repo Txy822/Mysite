06d788ac4f8e71f5b6adebf90ce0a78f11e3e61a
Txy822 <tyohanis2018@gmail.com>
2020-07-15 18:15:11 +0100

    functional tests added

--- cv/tests/test_functional.py
+++ cv/tests/test_functional.py
@@ -1,9 +1,30 @@
 from django.test import TestCase,Client
 from selenium import webdriver
+import unittest
 
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
+class FunctionalTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
 
-assert 'Tesfahun Curriculum Vitae' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-browser.quit()
+    def test_cv_page(self):
+        response = self.client.get('/cv')
+        self.assertEqual(response.status_code, 301)
+
+    def test_title_cv_page(self):
+        self.browser.get('http://localhost:8000/cv')
+        self.assertIn('Tesfahun Curriculum Vitae',self.browser.title)
+
+    def test_there_is_cv_page_name(self):
+        self.browser.get('http://localhost:8000/cv')
+        self.assertIn('Curriculum Vitae(CV)',self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
