7d527de9e773e7e0196cf3cf9b25943eac3b6736
Txy822 <tyohanis2018@gmail.com>
2020-08-11 21:17:25 +0100

    view for cv section edit added

--- cv/views.py
+++ cv/views.py
@@ -28,3 +28,17 @@ def cv_section_new(request):
     else:
         form = Cv_section_form()
     return render(request, 'cv/cv_section_edit.html', {'form': form})
+
+def cv_section_edit(request, pk):
+    cv_section = get_object_or_404(Cv_section, pk=pk)
+    if request.method == "POST":
+        form = Cv_section_form(request.POST, instance=cv_section)
+        if form.is_valid():
+            cv_section = form.save(commit=False)
+
+            # post.published_date = timezone.now()
+            cv_section.save()
+            return redirect('cv_section_detail', pk=cv_section.pk)
+    else:
+        form = Cv_section_form(instance=cv_section)
+    return render(request, 'cv/cv_section_edit.html', {'form': form})
