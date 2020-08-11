b417aa61e672c63fd26ad969970140290ec3568d
Txy822 <tyohanis2018@gmail.com>
2020-08-11 21:58:59 +0100

    urls  for cv section remove  added

--- cv/urls.py
+++ cv/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cv/<int:pk>/', views.cv_section_detail, name='cv_section_detail'),
     path('cv/new/', views.cv_section_new, name='cv_section_new'),
     path('cv/<int:pk>/edit/', views.cv_section_edit, name='cv_section_edit'),
+    path('cv/<pk>/remove/', views.cv_section_remove, name='cv_section_remove'),
     ]
