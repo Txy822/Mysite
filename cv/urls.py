c7b309e635952a1020a39cdbbe85e76ef4cc082c
Txy822 <tyohanis2018@gmail.com>
2020-08-11 21:11:23 +0100

    urls for  new cv section  added

--- cv/urls.py
+++ cv/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.cv_section_list, name='cv_section_list'),
     path('cv/<int:pk>/', views.cv_section_detail, name='cv_section_detail'),
+    path('cv/new/', views.cv_section_new, name='cv_section_new'),
     ]
