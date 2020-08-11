e213f3fd4bfd2a4253d7a38be146cc986f953a84
Txy822 <tyohanis2018@gmail.com>
2020-08-11 20:56:30 +0100

    test view for new cv section is addedd

--- cv/tests/test_views.py
+++ cv/tests/test_views.py
@@ -11,6 +11,7 @@ class TestViews(TestCase):
         self.client=Client()
         self.cv_section_list_url=reverse('cv_section_list')
         self.cv_section_detail_url=reverse('cv_section_detail',args=[1])
+        self.cv_section_new_url=reverse('cv_section_new')
 
         Cv_section.objects.create(
         title='title',
@@ -21,14 +22,21 @@ class TestViews(TestCase):
         response=self.client.get(self.cv_section_list_url)
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response,'cv/cv_sections_list.html')
+
+    def test_uses_correct_template(self):
+        response = self.client.get('/cv/')
+        self.assertTemplateUsed(response, 'cv/cv_sections_list.html')
+
     def test_cv_section_detail_GET(self):
         response=self.client.get(self.cv_section_detail_url)
         self.assertEquals(response.status_code,200)
         self.assertTemplateUsed(response,'cv/cv_section_detail.html')
 
-    def test_uses_correct_template(self):
-        response = self.client.get('/cv/')
-        self.assertTemplateUsed(response, 'cv/cv_sections_list.html')
+    def test_cv_section_new_GET(self):
+        response=self.client.get(self.cv_section_new_url)
+        self.assertIn('title', response.content.decode())
+        self.assertEquals(response.status_code,200)
+
 
     # def test_cv_page_returns_correct_html(self):
     #     response = self.client.get('/cv/')
