a01bc29331609daf50ee33f96113bb5bb2740485
Txy822 <tyohanis2018@gmail.com>
2020-08-17 06:03:20 +0100

    web app updated

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
